d3af7ae47712f69886a25f50e975d56c9c04623f
Izaiah <ijpen175@learner.dvusd.org>
2026-01-20 13:43:50 -0700

    p

--- Pie-Fieste/Pie-Fieste/Pie_Fieste.py
+++ Pie-Fieste/Pie-Fieste/Pie_Fieste.py
@@ -1,1 +1,6 @@
+print("\n-- Py-Fiest 2026 Stage Manager ---\n")
+print("1. view lineup & total time")
+print("2. add a new")
+print("3. Move First Band To End (Late Arrival)")
+print("4. Remove A Band by Name")
 
